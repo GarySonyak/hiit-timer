97c7bee7da9a6d37f13cf705d8981f12f8df4f91
GarySonyak <gary.sonyak@gmail.com>
2025-09-25 09:25:25 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE = 'hiit-timer-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE ? caches.delete(k) : null)))
+  );
+  self.clients.claim();
+});
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(resp => resp).catch(() => caches.match('./index.html')))
+  );
+});
